ff7ac6ff29fd97290f906e2fe44f0a3a0370d64b
ilmu23 <ivalimak@student.hive.fi>
2024-10-12 07:46:33 +0300

    Added ft_maparr

--- inc/lft_hmap.h
+++ inc/lft_hmap.h
@@ -6,7 +6,7 @@
 /*   By: ivalimak <ivalimak@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/21 02:07:12 by ivalimak          #+#    #+#             */
-/*   Updated: 2024/05/26 05:04:43 by ivalimak         ###   ########.fr       */
+/*   Updated: 2024/10/12 07:36:30 by ivalimak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,20 @@
 extern const uint8_t	g_hmap_sentinel;
 
 /*   alloc   */
-uint8_t	ft_mapadd(t_hmap *hmap, const char *key, const void *val);
+uint8_t		ft_mapadd(t_hmap *hmap, const char *key, const void *val);
 
-t_hmap	*ft_mapnew(void);
+t_hmap		*ft_mapnew(void);
+
+t_hmap_pair	*ft_maparr(const t_hmap *hmap);
 /*   alloc   */
 
 /*   no alloc   */
-uint8_t	ft_maprm(t_hmap *hmap, const char *key);
+uint8_t		ft_maprm(t_hmap *hmap, const char *key);
 
-void	*ft_mapget(t_hmap *hmap, const char *key);
+void		*ft_mapget(t_hmap *hmap, const char *key);
 
-void	ft_mappop(t_hmap_pair *pair);
-void	ft_mappop_all(t_hmap *hmap);
+void		ft_mappop(t_hmap_pair *pair);
+void		ft_mappop_all(t_hmap *hmap);
 /*   no alloc   */
 
 #endif
